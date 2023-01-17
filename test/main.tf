df491f68826989e9af37cea150bfa76fed199ee5
Nikcoder <43770346+Nikcoder@users.noreply.github.com>
2023-01-17 13:50:34 +0530

    Update main.tf

--- test/main.tf
+++ test/main.tf
@@ -1,18 +1,18 @@
 module "vpc" {
   source            = "../modules/vpc"
-  vpc_cidr          = "10.0.0.0/16"
+  vpc_cidr          = "10.1.0.0/16"
   vpc_name          = "Terraform_vpc"
-  subnet_cidr       = "10.0.1.0/24"
+  subnet_cidr       = "10.1.12.0/24"
   az_zone1          = "us-east-2a"
-  pri_subnet_cidr   = "10.0.2.0/24"
+  pri_subnet_cidr   = "10.1.24.0/24"
   pub_subnet_name   = "Terraform_public_subnet"
   pri_subnet_name   = "Terraform_private_subnet"
   vpc_id            = "module.vpc.vpc_id"
   az_zone2          = "us-east-2b"
   pub_subnet_name_2 = "Terraform_public_subnet_2"
-  pub_subnet_cidr   = "10.0.3.0/24"
+  pub_subnet_cidr   = "10.1.48.0/24"
   pri_subnet_name2  = "Terraform_private_subnet_2"
-  pri_subnet_cidr2  = "10.0.4.0/24"
+  pri_subnet_cidr2  = "10.1.56.0/24"
 }
 
 module "InternetGW" {
